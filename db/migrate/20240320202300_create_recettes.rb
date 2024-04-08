dc5a42df4db3364bfc5a59401ac135804b0a90c3
toba75 <github@mfbougies.com>
2024-04-08 21:29:45 +0200

    ajout de personnes à Recette

--- db/migrate/20240320202300_create_recettes.rb
+++ db/migrate/20240320202300_create_recettes.rb
@@ -3,6 +3,7 @@ class CreateRecettes < ActiveRecord::Migration[7.1]
     create_table :recettes do |t|
       t.string :nom
       t.integer :temps
+      t.integer :personnes
       t.string :nutriscore
 
       t.timestamps
